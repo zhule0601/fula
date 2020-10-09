fb591779320ced65e010604ee408ce50a2606b1b
zhule <zhuleaiziji@163.com>
2020-10-09 09:33:46 +0800

    add auto upgrade server shell script (#9)

--- server/bin/auto/auto-upgrade-fula.sh
+++ server/bin/auto/auto-upgrade-fula.sh
@@ -1,10 +1,16 @@
-########################
-# 1. 替换配置文件
-# 2. stop
-# 3. start
-#
-#
-#
-#
-#
-########################
+#!/bin/bash
+# 1. unzip
+# 2. copy application.properties
+# 3. restart server
+
+base_dir=$(cd "$(dirname "$0")" || exit; pwd)
+echo "$base_dir"
+
+tar_name=$(ls -lt "/opt/build" | grep "fula" | grep "tar" | head -n 1 | awk '{print $9}')
+fula_name=${tar_name%%.*}
+echo "$tar_name"
+echo "$fula_name"
+tar -zvxf "$base_dir"/"$tar_name" -C "$base_dir"
+\cp /opt/build/application.properties "$base_dir"/"$fula_name"/conf/
+
+exec "$base_dir"/"$fula_name"/bin/fula-server.sh restart
