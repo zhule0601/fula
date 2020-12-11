54c475179b1290fd734a513eac115814bef481cb
zhule0601 <zhule0601@gmail.com>
2020-12-11 12:47:55 +0800

    tg docker 镜像更新

--- docker/tg-client/tg.sh
+++ docker/tg-client/tg.sh
@@ -4,12 +4,12 @@ case $1 in
 send)
   echo "send_file"
   echo "/opt/tg_client/bin/telegram-cli -W -e "send_file $2 $3""
-  /root/tg/bin/telegram-cli -W -e "send_file @$2 $3"
+  docker exec telegram /bin/bash -c "send_file @$2 $3"
   ;;
 msg)
   echo "msg"
-  echo "/opt/tg_client/bin/telegram-cli -W -e "msg @$2 $3""
-  /opt/tg_client/bin/telegram-cli -W -e "msg @$2 $3"
+  echo "/docker exec telegram /bin/bash -c tg msg "@$2 $3""
+  docker exec telegram /bin/bash -c "tg msg @$2 $3"
   ;;
 *)
   echo "send|msg"
