641ca7f55ee5d898963da70c1e259ec234fd6644
zhule0601 <zhule0601@gmail.com>
2021-02-03 17:00:26 +0800

    初始化 福鼎家园-放炮 页面

---
+++ server/src/main/java/com/fula/thymeleaf/IndexController.java
@@ -0,0 +1,43 @@
+package com.fula.thymeleaf;
+
+import com.fula.model.dto.SearchCustomerInfoDTO;
+import com.fula.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+public class IndexController {
+
+  @Autowired CustomerService customerService;
+
+  @RequestMapping("/")
+  public String toIndex() {
+    return "index";
+  }
+
+  @RequestMapping("/test")
+  public ModelAndView test(ModelAndView modelAndView) {
+    modelAndView.addObject("name", "test");
+    modelAndView.setViewName("test");
+    return modelAndView;
+  }
+
+  @RequestMapping("/users")
+  public ModelAndView users(ModelAndView modelAndView) {
+    List<SearchCustomerInfoDTO> customerInfoList =
+        customerService.search(new SearchCustomerInfoDTO());
+    modelAndView.addObject("users", customerInfoList);
+    modelAndView.setViewName("user");
+    return modelAndView;
+  }
+
+  @RequestMapping("/fuding")
+  public ModelAndView fuding(ModelAndView modelAndView) {
+    modelAndView.setViewName("fuding");
+    return modelAndView;
+  }
+}
